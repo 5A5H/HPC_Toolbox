32d33a162c6b0e5f19449740cb03aaa3651b50fd
Sascha Maassen <sascha.maassen@uni-due.de>
2022-03-24 13:28:35 +0100

    wtime tracking

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 # include <iostream>
 # include <mpi.h>
 
-int main ( int argc, char *argv[] )
+int main( int argc, char *argv[] )
 {
     int ierr, size, rank, master(0);
     double wall_time;
 
     // initialize mpi communicator
-    ierr = MPI_Init ( &argc, &argv );
+    ierr = MPI_Init( &argc, &argv );
     if (ierr!=0) std::cerr << "Error! Could not initialize MPI Communicator." << std::endl;
 
     // identify ranks
-    MPI_Comm_size ( MPI_COMM_WORLD, &size );
-    MPI_Comm_rank ( MPI_COMM_WORLD, &rank );
+    MPI_Comm_size( MPI_COMM_WORLD, &size );
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 
     // master rank tracks time
     if (rank==master) wall_time = MPI_Wtime();
@@ -20,7 +20,12 @@ int main ( int argc, char *argv[] )
     // every rank sents a message
     std::cout << "Hello from rank " << rank << " out of " << size << std::endl;
 
+    // track the time
+    MPI_Barrier( MPI_COMM_WORLD );
+    if (rank==master) std::cout << "Elapsed wall clock time: " << MPI_Wtime ( ) - wall_time << " seconds." << std::endl;
+
     // finalize mpi communicator
     MPI_Finalize();
+
 
 }
